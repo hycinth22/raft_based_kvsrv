e06c624860b32ec0583be7cdf6ba7045fc098791
听寒 <10476912+hycinth22@users.noreply.github.com>
2025-06-21 03:16:33 +0800

    log file

--- src/raft1/util.go
+++ src/raft1/util.go
@@ -1,6 +1,8 @@
 package raft
 
 import "log"
+import "os"
+import "bufio"
 
 // Debugging
 const Debug = false
@@ -9,4 +11,13 @@ func DPrintf(format string, a ...interface{}) {
 	if Debug {
 		log.Printf(format, a...)
 	}
+}
+
+func init() {
+    file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatal("无法打开日志文件:", err)
+    }
+	writer := bufio.NewWriter(file)
+	log.SetOutput(writer)
 }
